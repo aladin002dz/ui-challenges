253abbe64d2e87f8d20e3f764a33371785e824ec
aladin002dz <mahfoudh.arous@gmail.com>
2020-11-04 16:38:46 +0100

    update share section display on desktop

--- 06-article-preview-component-master/main.js
+++ 06-article-preview-component-master/main.js
@@ -1,4 +1,5 @@
-// Toggle Sping Class
+// Toggle Display Class
 document.querySelector(".share-btn").onclick = function() {
     document.querySelector(".social-link").classList.toggle("display-none");
+    //document.querySelector(".author").classList.toggle("display-none-mobile");
 }
