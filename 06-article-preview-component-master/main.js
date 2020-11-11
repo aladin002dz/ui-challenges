bd413ff2dc3853d62c3a885d198490d83b288dfd
aladin002dz <mahfoudh.arous@gmail.com>
2020-11-11 22:22:35 +0100

    working on card footer display on mobile

--- 06-article-preview-component-master/main.js
+++ 06-article-preview-component-master/main.js
@@ -1,5 +1,7 @@
 // Toggle Display Class
 document.querySelector(".share-btn").onclick = function() {
     document.querySelector(".social-link").classList.toggle("display-none");
-    //document.querySelector(".author").classList.toggle("display-none-mobile");
+
+    // on mobile display
+    document.querySelector(".author").classList.toggle("display-none-mobile");
 }
