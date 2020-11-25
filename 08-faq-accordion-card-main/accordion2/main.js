6795ba955272c08a194a587b421ee5cb2694fe0c
aladin002dz <mahfoudh.arous@gmail.com>
2020-11-25 13:59:43 +0100

    challenge08: accordion prototyping II

--- 08-faq-accordion-card-main/accordion2/main.js
+++ 08-faq-accordion-card-main/accordion2/main.js
@@ -1,23 +1,10 @@
-/*document.querySelector(".tab").onclick = function () {
-    let x = this.parentElement.nodeName;
-    x.classList.toggle("open");
-}*/
-
 let tabs = document.querySelectorAll(".tab");
-console.log(tabs);
 
-function openAccordion(e) {
+function openCurrAccordion(e) {
     let parent = this.parentElement;
     parent.classList.toggle("open");
 }
 
-tabs.forEach((tab) => {tab.addEventListener('click', openCurrAccordion);});
-
-
-
-/*
-document.querySelector(".tab").onclick = function() {
-    var x = document.getElementById("myLI").parentElement.nodeName;
-    document.querySelector(".tab").classList.toggle("open");
-}
-*/
+tabs.forEach((tab) => {
+    tab.childNodes[1].addEventListener('click', openCurrAccordion);
+});
