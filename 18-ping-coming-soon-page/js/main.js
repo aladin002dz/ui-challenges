3fd099c554d230d7488869fe4b127c0cc644202e
aladin002dz <mahfoudh.arous@gmail.com>
2022-03-24 11:44:47 +0100

    ch18: adding a toast

--- 18-ping-coming-soon-page/js/main.js
+++ 18-ping-coming-soon-page/js/main.js
@@ -8,5 +8,19 @@ function validateForm() {
     return;
   }
   document.getElementById("mail-form").classList.remove("invalid");
+  /* Display a validation toast for the user***************************************/
+  /*code snnipet from :https://www.w3schools.com/howto/howto_js_snackbar.asp*/
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function () {
+    x.className = x.className.replace("show", "");
+    document.getElementById("email").value = "";
+  }, 3000);
+
   console.log("valid!");
 }
