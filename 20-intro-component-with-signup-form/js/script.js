90b3a434484853aaf1a2a76b32cdc3f13731b1bf
aladin002dz <mahfoudh.arous@gmail.com>
2022-05-08 14:31:13 +0100

    ch20: complete validation

--- 20-intro-component-with-signup-form/js/script.js
+++ 20-intro-component-with-signup-form/js/script.js
@@ -1,3 +1,5 @@
+//credit to https://github.com/Lakorthus/FrontendMentorChallenge/blob/main/intro-component-with-signup-form-master/script.js
+//credit to https://github.com/Lakorthus/FrontendMentorChallenge/blob/main/intro-component-with-signup-form-master/script.js
 const form = document.getElementById("form1");
 
 form.addEventListener("submit", (e) => {
@@ -9,12 +11,16 @@ form.addEventListener("submit", (e) => {
 
   if (firstname === "") {
     addErrorTo("firstname", "First Name is required");
+  } else if (!isValid(firstname, nameRegexPattern)) {
+    addErrorTo("firstname", "firstname is not valid");
   } else {
     removeErrorFrom("firstname");
   }
 
   if (lastname === "") {
     addErrorTo("lastname", "Last Name is required");
+  } else if (!isValid(lastname, nameRegexPattern)) {
+    addErrorTo("lastname", "lastname is not valid");
   } else {
     removeErrorFrom("lastname");
   }
@@ -29,12 +35,13 @@ form.addEventListener("submit", (e) => {
 
   if (password === "") {
     addErrorTo("password", "Password is required");
+  } else if (!isValid(email, emailRegexPattern)) {
+    addErrorTo("password", "password is not valid");
   } else {
     removeErrorFrom("password");
   }
 });
 
-//credit to https://github.com/Lakorthus/FrontendMentorChallenge/blob/main/intro-component-with-signup-form-master/script.js
 function addErrorTo(field, message) {
   console.log("field: ", field);
   console.log("message: ", message);
@@ -42,6 +49,7 @@ function addErrorTo(field, message) {
   formControl.classList.add("error");
 
   const small = formControl.querySelector("small");
+  console.log("small: ", small);
   small.innerText = message;
 }
 
@@ -50,20 +58,20 @@ function removeErrorFrom(field) {
   formControl.classList.remove("error");
 }
 
-const emailRegexPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
 function isValid(field, regexPattern) {
   return regexPattern.test(field);
 }
 
-/*
-  let emailInput = document.getElementById("email").value;
-  const emailRegexPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (!emailRegexPattern.test(emailInput)) {
-    document.getElementById("form1").classList.add("invalid");
-    console.log("invalid!");
-    return;
-  }
-  document.getElementById("form1").classList.remove("invalid");
-  console.log("valid!");
-  */
+//My Regex patterns
+const nameRegexPattern = /^[A-Z][A-Za-zéç]+(\s[A-Z][A-Za-zéç]+)*$/;
+const emailRegexPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+/* 
+  Credit:
+  https://stackoverflow.com/questions/16965953/regular-expressions-for-username-and-password
+  (?=..)    is a lookahead that don't consume characters but only check
+  (?=[^a-z]*[a-z]) check if there is at least 1 lower case letter 
+  (?=\D*\d)   check if there is at least 1 digit
+  [^:&.~\s]  a character class that exclude all the characters you don't want
+*/
+const passwprdRegexPattern = /^(?=[^a-z]*[a-z])(?=\D*\d)[^:&.~\s]{5,20}$/;
