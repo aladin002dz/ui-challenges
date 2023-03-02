aec725a278b70bbedaf0ee795317ecf3e9e5a4e7
aladin002dz <mahfoudh.arous@gmail.com>
2023-03-02 11:26:10 +0100

    Ch21: submenus open hide

--- 21-intro-section-with-dropdown-navigation/js/index.js
+++ 21-intro-section-with-dropdown-navigation/js/index.js
@@ -2,3 +2,24 @@ document.querySelector('.navbar-toggle').addEventListener('click', function () {
     const navbar = document.querySelector('header');
     navbar.classList.toggle('is-menu-open')
 });
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const dropdownToggle = dropdown.querySelector('a');
+    const dropdownMenu = dropdown.querySelector('.dropdown-menu');
+
+    dropdownToggle.addEventListener('click', () => {
+        dropdown.classList.toggle('open');
+    });
+
+    dropdownMenu.addEventListener('click', (event) => {
+        event.stopPropagation();
+    });
+
+    document.addEventListener('click', (event) => {
+        if (!dropdown.contains(event.target)) {
+            dropdown.classList.remove('open');
+        }
+    });
+});
