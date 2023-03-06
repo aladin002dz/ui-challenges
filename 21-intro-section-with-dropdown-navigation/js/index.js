758d9b7a741e8d95c5b9e70e4ec32ceea75adfa5
aladin002dz <mahfoudh.arous@gmail.com>
2023-03-06 13:47:31 +0100

    Ch21: adding and setting aria attributes in header

--- 21-intro-section-with-dropdown-navigation/js/index.js
+++ 21-intro-section-with-dropdown-navigation/js/index.js
@@ -1,17 +1,23 @@
 document.querySelector('.navbar-toggle').addEventListener('click', function () {
     const navbar = document.querySelector('header');
     navbar.classList.toggle('is-menu-open')
+    this.setAttribute('aria-expanded', this.getAttribute('aria-expanded') === 'false' ? 'true' : 'false');
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    navbarCollapse.setAttribute('aria-hidden', navbarCollapse.getAttribute('aria-hidden') === 'true' ? 'false' : 'true');
 });
 
-const dropdowns = document.querySelectorAll('.dropdown');
+const dropdowns = document.querySelectorAll('.dropdown-toggle');
 dropdowns.forEach(dropdown => {
-    const dropdownMenu = dropdown.querySelector('.dropdown-menu');
+    const dropdownMenu = dropdown.nextElementSibling;
     dropdown.addEventListener('click', () => {
         dropdown.classList.toggle('open');
+        dropdown.setAttribute('aria-expanded', dropdown.getAttribute('aria-expanded') === 'false' ? 'true' : 'false');
         if (dropdown.classList.contains('open')) {
             dropdownMenu.style.maxHeight = dropdownMenu.scrollHeight + 'px';
+            dropdownMenu.setAttribute('aria-hidden', 'false');
         } else {
             dropdownMenu.style.maxHeight = 0;
+            dropdownMenu.setAttribute('aria-hidden', 'true');
         }
     });
 });
