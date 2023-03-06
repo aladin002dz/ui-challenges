735488b6c4637fa5add7ce5d095949aaa0a3c1bb
aladin002dz <mahfoudh.arous@gmail.com>
2023-03-06 11:45:23 +0100

    Ch21: finalizing on submenus effects

--- 21-intro-section-with-dropdown-navigation/js/index.js
+++ 21-intro-section-with-dropdown-navigation/js/index.js
@@ -4,33 +4,14 @@ document.querySelector('.navbar-toggle').addEventListener('click', function () {
 });
 
 const dropdowns = document.querySelectorAll('.dropdown');
-
 dropdowns.forEach(dropdown => {
-    //const dropdownToggle = dropdown.querySelector('.dropdown');
     const dropdownMenu = dropdown.querySelector('.dropdown-menu');
-    /*
-        dropdownToggle.addEventListener('click', () => {
-            dropdown.classList.toggle('open');
-        });*/
     dropdown.addEventListener('click', () => {
         dropdown.classList.toggle('open');
         if (dropdown.classList.contains('open')) {
             dropdownMenu.style.maxHeight = dropdownMenu.scrollHeight + 'px';
         } else {
             dropdownMenu.style.maxHeight = 0;
-        }
-    });
-    /*
-        dropdownMenu.addEventListener('click', (event) => {
-            event.stopPropagation();
-        });
-    */
-    //add max height to dropdown menu scrollHeight
-
-
-    document.addEventListener('click', (event) => {
-        if (!dropdown.contains(event.target)) {
-            dropdown.classList.remove('open');
         }
     });
 });
