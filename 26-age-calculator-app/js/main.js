710fc93eae69e7f217b05b8e3019e650e6151d1d
aladin002dz <mahfoudh.arous@gmail.com>
2023-06-18 16:41:43 +0100

    adding form validation

--- 26-age-calculator-app/js/main.js
+++ 26-age-calculator-app/js/main.js
@@ -4,6 +4,14 @@ function main() {
     formCalculateAge.addEventListener("submit", calculateAge);
     function calculateAge(e) {
         e.preventDefault();
+        document.getElementById("years-counter").textContent = "--";
+        document.getElementById("months-counter").textContent = "--";
+        document.getElementById("days-counter").textContent = "--";
+
+        if (!validateForm()) {
+            return;
+        }
+
         const birthDate = new Date(document.getElementById("year-of-birth").value, document.getElementById("month-of-birth").value - 1, document.getElementById("day-of-birth").value);
         const today = new Date();
         const ageInMilliseconds = today - birthDate;
@@ -18,4 +26,56 @@ function main() {
         document.getElementById("months-counter").textContent = Math.floor(ageInMonths);
         document.getElementById("days-counter").textContent = ageInDays;
     }
+}
+
+function validateForm() {
+    const yearInput = document.getElementById("year-of-birth");
+    const monthInput = document.getElementById("month-of-birth");
+    const dayInput = document.getElementById("day-of-birth");
+
+    const year = yearInput.value;
+    const month = monthInput.value;
+    const day = dayInput.value;
+
+
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    var yearPattern = /^[1-9]\d*$/;
+    var monthPattern = /^(0?[1-9]|1[0-2])$/;
+    var dayPattern = /^(0?[1-9]|[1-2]\d|3[0-1])$/;
+
+    let valid = true;
+
+    if (!yearPattern.test(year) || year > currentYear) {
+        addInvalidClassToInputGroup(yearInput);
+        valid = false;
+    } else {
+        removeInvalidClassFromInputGroup(yearInput);
+    }
+
+    if (!monthPattern.test(month)) {
+        addInvalidClassToInputGroup(monthInput);
+        valid = false;
+    } else {
+        removeInvalidClassFromInputGroup(monthInput);
+    }
+
+    if (!dayPattern.test(day)) {
+        addInvalidClassToInputGroup(dayInput);
+        valid = false;
+    } else {
+        removeInvalidClassFromInputGroup(dayInput);
+    }
+
+    return valid;
+}
+
+function addInvalidClassToInputGroup(element) {
+    const parent = element.closest('.input-group');
+    parent.classList.add('error');
+}
+
+function removeInvalidClassFromInputGroup(element) {
+    const parent = element.closest('.input-group');
+    parent.classList.remove('error');
 }
