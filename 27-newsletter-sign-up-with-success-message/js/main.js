c43879cd7515446cd0a44cf39d98661cc2946890
aladin002dz <mahfoudh.arous@gmail.com>
2023-07-25 15:56:56 +0100

    Ch27: adding email validation

--- 27-newsletter-sign-up-with-success-message/js/main.js
+++ 27-newsletter-sign-up-with-success-message/js/main.js
@@ -5,6 +5,10 @@ function main() {
     const signupForm = document.querySelector('#signup-form');
     signupForm.addEventListener('submit', function (e) {
         e.preventDefault();
+        if (!validateEmail()) {
+            return;
+        }
+
         singupSection.classList.toggle('hidden');
         successSection.classList.toggle('hidden');
     });
@@ -14,4 +18,19 @@ function main() {
         successSection.classList.toggle('hidden');
         singupSection.classList.toggle('hidden');
     });
+}
+
+function validateEmail() {
+    var emailInput = document.getElementById("email").value;
+    var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!emailPattern.test(emailInput)) {
+        var errorMessage = document.getElementById("error-message");
+        errorMessage.textContent = "Valid email required";
+
+        emailInput.classList.add("error");
+        return false; // Prevent form submission
+    }
+    emailInput.classList.remove("error");
+    return true; // Allow form submission
 }
